785398ff2472859bb1d8783156f4515fd67971f9
Ash Williams <housewares@users.noreply.github.com>
2021-01-09 20:21:37 +0300

    Add script to get agent version in rancher/server images

---
+++ dist/server-and-agent-versions.sh
@@ -0,0 +1,69 @@
+# This info is gathered from rancher/rancher GitHub releases:
+# https://github.com/rancher/rancher/releases
+
+# https://github.com/rancher/rancher/releases/v1.6.30
+# rancher/server:v1.6.30
+# rancher/agent:v1.2.11
+# rancher/lb-service-haproxy:v0.9.14
+
+# https://github.com/rancher/rancher/releases/v1.6.30
+# rancher/server:v1.6.29
+# rancher/agent:v1.2.11
+# rancher/lb-service-haproxy:v0.9.13
+
+# https://github.com/rancher/rancher/releases/v1.6.28
+# rancher/server:v1.6.28
+# rancher/agent:v1.2.11
+# rancher/lb-service-haproxy:v0.9.11
+
+# https://github.com/rancher/rancher/releases/v1.6.27
+# rancher/server:v1.6.27
+# rancher/agent:v1.2.11
+# rancher/lb-service-haproxy:v0.9.11
+
+# https://github.com/rancher/rancher/releases/v1.6.26
+# rancher/server:v1.6.26
+# rancher/agent:v1.2.11
+# rancher/lb-service-haproxy:v0.9.6
+
+# https://github.com/rancher/rancher/releases/v1.6.25
+# rancher/server:v1.6.25
+# rancher/agent:v1.2.11
+# rancher/lb-service-haproxy:v0.9.6
+
+# set -x
+
+# Get built-in agent version in server image
+
+printf -v dash '%.0s-' {1..80}
+
+echo $dash
+echo '  [ rancher/server ]'
+echo $dash
+
+range=30
+for ((i=25; i<=range ; i++))
+do
+    image='rancher/server'
+    tag="v1.6.${i}"
+    server_agent_path='/usr/share/cattle/artifacts/go-agent.tar.gz'
+    tmp_file="$(mktemp)"
+    digest="$(docker pull "${image}:${tag}" | grep -oP '\Ksha256:\w+$')"
+
+    docker run --rm --entrypoint tar "${image}@${digest}" \
+      --extract --to-stdout --wildcards --no-anchored 'agent' --file="${server_agent_path}" > "${tmp_file}"
+
+    chmod +x "${tmp_file}"
+
+    version="$("${tmp_file}" --version | awk '{print $3}')"
+
+    printf "Server image  : %s\nImage digest  : %s\nAgent version : %s\nAgent git log : %s\n" \
+      "${image}:${tag}" \
+      "${digest}" \
+      "${version}" \
+      "$(git rev-list --format=%n%B%n%n%D --max-count=1 "${version/-dirty/}")"
+
+    rm "${tmp_file}"
+
+    echo $dash
+done
